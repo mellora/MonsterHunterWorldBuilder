9f0d5920540f80b9d271d05bf5eb12f250d1b37d
Alexander Mellor <AlexMellor2014@gmail.com>
2020-06-01 16:27:37 -0500

    Started test file for repository testing

---
+++ src/test/java/com/mellora/mhw/builder/BuilderRepoTests.java
@@ -0,0 +1,16 @@
+package com.mellora.mhw.builder;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+
+@JdbcTest
+class BuilderRepoTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
